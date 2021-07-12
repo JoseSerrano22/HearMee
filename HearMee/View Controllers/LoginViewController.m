4949d3953b2b56fd391f30b117f3ad665d343434
Jose Serrano <jose.serrano22@upr.edu>
2021-07-12 17:07:57 -0400

    Login Functionality Complete

--- HearMee/View Controllers/LoginViewController.m
+++ HearMee/View Controllers/LoginViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/Parse.h"
 
-@interface LoginViewController ()
+@interface LoginViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -15,17 +19,112 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.usernameField.delegate = self;
+    self.passwordField.delegate = self;
+    
+    UITapGestureRecognizer *const tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_dismissKeyboard)];
+    [self.view addGestureRecognizer:tap];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Private
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)_signUpDidTap:(id)sender {
+    if (![self _isTextFieldEmpty]){
+        [self _registerUser];
+    }
 }
-*/
+
+- (IBAction)_logInDidTap:(id)sender {
+    if (![self _isTextFieldEmpty]){
+        [self _loginUser];
+    }
+}
+
+- (void)_registerUser {
+    PFUser *const newUser = [PFUser user];
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            [self performSegueWithIdentifier:@"tabBarSegue" sender:nil];
+        }
+    }];
+}
+
+- (void)_loginUser {
+    NSString *const username = self.usernameField.text;
+    NSString *const password = self.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            [self performSegueWithIdentifier:@"tabBarSegue" sender:nil];
+        }
+    }];
+}
+
+-(void)_dismissKeyboard {
+    [self.usernameField resignFirstResponder];
+    [self.passwordField resignFirstResponder];
+}
+
+- (BOOL)_isTextFieldEmpty {
+    
+    BOOL flag = FALSE;
+    
+    if ([self.usernameField.text isEqual:@""]) {
+        UIAlertController *const usernameAlert = [UIAlertController alertControllerWithTitle:@"Title"
+                                                                                     message:@"Is empty the username"
+                                                                              preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *const cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                                     style:UIAlertActionStyleCancel
+                                                                   handler:^(UIAlertAction * _Nonnull action) {
+        }];
+        [usernameAlert addAction:cancelAction];
+        UIAlertAction *const okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                                 style:UIAlertActionStyleDefault
+                                                               handler:^(UIAlertAction * _Nonnull action) {
+        }];
+        [usernameAlert addAction:okAction];
+        [self presentViewController:usernameAlert animated:YES completion:^{
+        }];
+        
+        flag = TRUE;
+        
+    }
+    else if ([self.passwordField.text isEqual:@""]){
+        UIAlertController *const passwordAlert = [UIAlertController alertControllerWithTitle:@"Title"
+                                                                                     message:@"Is empty the password"
+                                                                              preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *const cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                                     style:UIAlertActionStyleCancel
+                                                                   handler:^(UIAlertAction * _Nonnull action) {
+        }];
+        [passwordAlert addAction:cancelAction];
+        UIAlertAction *const okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                                 style:UIAlertActionStyleDefault
+                                                               handler:^(UIAlertAction * _Nonnull action) {
+        }];
+        [passwordAlert addAction:okAction];
+        [self presentViewController:passwordAlert animated:YES completion:^{
+        }];
+        flag = TRUE;
+    }
+    
+    return flag;
+}
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
 
 @end
