21b47440a50ad80545f1cef1e3a13f295b7cff79
Jose Serrano <jose.serrano22@upr.edu>
2021-07-15 16:36:19 -0400

    created logo and started with the chat's functions

---
+++ HearMee/Views/ConversationsCell.h
@@ -0,0 +1,16 @@
+//
+//  ConversationsCell.h
+//  HearMee
+//
+//  Created by jose1009 on 7/15/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConversationsCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HearMee/Views/ConversationsCell.m
@@ -0,0 +1,23 @@
+//
+//  ConversationsCell.m
+//  HearMee
+//
+//  Created by jose1009 on 7/15/21.
+//
+
+#import "ConversationsCell.h"
+
+@implementation ConversationsCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
